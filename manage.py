5a8c72635694659dc152dc55f6b618540860d401
DongHongfei <1247543758@qq.com>
2024-09-05 21:48:45 +0800

    bugfix

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
+import os
+
 from app.log import logger
 from app.notion_manager import get_active_rss_feeds, save_article_to_notion
 from app.rss_fetcher import fetch_rss_content
 
 
 def main():
+    # 打印所有环境变量
+    logger.debug("Printing all environment variables:")
+    for key, value in os.environ.items():
+        logger.debug(f"{key}: {value}")
 
     # 读取RSS数据库，获取启用的RSS Feed
     active_rss_feeds = get_active_rss_feeds()
